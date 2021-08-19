dafa0b132069bf89b07a5a2726f5bebfe0665bb2
SeaSnake10 <76809064+SeaSnake10@users.noreply.github.com>
2021-08-19 15:56:25 +0800

    Add files via upload

---
+++ Screens/in.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class In extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>
+                    Instagram
+                </Text>
+            </View>
+        );
+    }
+}
